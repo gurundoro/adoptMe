c41b5dc29364e1d6cc6cd764d4b15e263bc73bbb
kudzanayi <kdzvairo@gmail.com>
2020-06-24 11:50:50 -0400

    add reach router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Pet from './Pet';
+import { Router, Link } from "@reach/router"
+import SearchParams from './SerachParams';
+import Details from './Details';
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Havanese'
-    }),
-    React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'Bird',
-      breed: 'Cockatiel'
-    }),
-    React.createElement(Pet, { name: 'Doink', animal: 'Cat', breed: 'Mix' })
-  ]);
+
+
+    return (
+        <React.StrictMode>
+        <div>
+          <header>
+            <Link to="/"> Adopt ME</Link>
+          </header>
+            <Router>
+                <SearchParams path= "/" />
+                <Details path="/details/1" />
+             </Router>
+        </div>
+        </React.StrictMode>
+    )
 };
 
-render(React.createElement(App), document.getElementById('root'))
+render(<App />, document.getElementById('root'))
